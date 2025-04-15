867c97f2dae4bf4cfd1f592092138c072da28142
Korey Hawsey <bhawsey13@gmail.com>
2025-04-15 12:56:33 +0100

    download note pdf fixes 1.3

--- directory.py
+++ directory.py
@@ -349,7 +349,7 @@ def accountDetailsAdminControl(username):
 
 @app.route("/downloadNote/<noteID>", methods=['GET', 'POST'])
 def downloadNote(noteID):
-    note = notes.find_one({'_id': ObjectID(noteID)})
+    note = notes.find_one({'_id': ObjectId(noteID)})
     user = users.find_one({'username': session.get('username', None)})
     #if note == None:
         #return redirect(url_for('home'))
