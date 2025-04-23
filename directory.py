08016a6c87c098bb9ec50fc3f7c6fd33835b0def
Korey Hawsey <bhawsey13@gmail.com>
2025-04-23 15:56:44 +0100

    download note pdf fixes 2.1.12

--- directory.py
+++ directory.py
@@ -362,7 +362,7 @@ def downloadNote(noteID):
     content = note['content']
     noteName = note['name']
 
-    config = pdfkit.configuration(wkhtmltopdf = 'wkhtmltopdf.exe')
+    config = pdfkit.configuration(wkhtmltopdf = 'wkhtmltopdf')
     pdf = pdfkit.from_string(content, 'downloads/' + noteName + '.pdf', configuration=config, verbose=True)
 
     return redirect(url_for('viewNote', noteID=noteID))
