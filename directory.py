cab2b72bea9bcac523b2790ae98c00c18a5b4b31
Korey Hawsey <bhawsey13@gmail.com>
2025-04-23 14:47:01 +0100

    download note pdf fixes 2.1.4

--- directory.py
+++ directory.py
@@ -362,8 +362,8 @@ def downloadNote(noteID):
     content = note['content']
     noteName = note['name']
 
-    config = pdfkit.configuration(wkhtmltopdf="/wkhtmltopdf.exe")   #for local hosting must be /public/wkhtmltopdf but is copied to root on vercel when it is hosted online
-    pdf = pdfkit.from_string(content, noteName + '.pdf', configuration=config)
+    config = pdfkit.configuration(wkhtmltopdf = url_for('public', filename='wkhtmltopdf.exe'))
+    pdf = pdfkit.from_string(content, 'downloads/' + noteName + '.pdf', configuration=config)
 
     return redirect(url_for('viewNote', noteID=noteID))
 
