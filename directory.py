cda83c13d87ad2c3261be9f3200128e44b1b0065
Korey Hawsey <bhawsey13@gmail.com>
2025-04-24 15:52:29 +0100

    summarize note fixes 1.5.6

--- directory.py
+++ directory.py
@@ -391,11 +391,14 @@ def summarizeNote(noteID):
 
     name = note['name']
     #content = note['content']
-    content = """ Summarization
-    Condensing a large paragrpah down into a smaller more digestable form.
-    Usually should be between four to six sentenses or around one hundred words.
-    Should not include specific details such as dates or miniscule information.
-    Should be broad enough to encompass the whole point.
+    content = """ Extraction-based summarization
+Here, content is extracted from the original data, but the extracted content is not modified in any way. Examples of extracted content include key-phrases that can be used to "tag" or index a text document, or key sentences (including headings) that collectively comprise an abstract, and representative images or video segments, as stated above. For text, extraction is analogous to the process of skimming, where the summary (if available), headings and subheadings, figures, the first and last paragraphs of a section, and optionally the first and last sentences in a paragraph are read before one chooses to read the entire document in detail.[10] Other examples of extraction that include key sequences of text in terms of clinical relevance (including patient/problem, intervention, and outcome).[11]
+
+Abstractive-based summarization
+Abstractive summarization methods generate new text that did not exist in the original text.[12] This has been applied mainly for text. Abstractive methods build an internal semantic representation of the original content (often called a language model), and then use this representation to create a summary that is closer to what a human might express. Abstraction may transform the extracted content by paraphrasing sections of the source document, to condense a text more strongly than extraction. Such transformation, however, is computationally much more challenging than extraction, involving both natural language processing and often a deep understanding of the domain of the original text in cases where the original document relates to a special field of knowledge. "Paraphrasing" is even more difficult to apply to images and videos, which is why most summarization systems are extractive.
+
+Aided summarization
+Approaches aimed at higher summarization quality rely on combined software and human effort. In Machine Aided Human Summarization, extractive techniques highlight candidate passages for inclusion (to which the human adds or removes text). In Human Aided Machine Summarization, a human post-processes software output, in the same way that one edits the output of automatic translation by Google Translate.
     """
 
     #parser = HtmlParser.from_string(content, Tokenizer("english"))
@@ -403,7 +406,7 @@ def summarizeNote(noteID):
     #stemmer = Stemmer("english")
     summarizer = LsaSummarizer()
     #summarizer.stop_words = get_stop_words("english")
-    summary = summarizer(parser.document, sentences_count=2)  # You can adjust the number of sentences in the summary
+    summary = summarizer(parser.document, sentences_count=4)  # You can adjust the number of sentences in the summary
 
     return render_template("viewSummary.html", summary=summary, name=name)
 
