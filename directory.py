0f3e40a51c2065ba436416cc213ea5307caa5d37
Korey Hawsey <bhawsey13@gmail.com>
2025-04-24 22:42:01 +0100

    add mapping template

--- directory.py
+++ directory.py
@@ -81,14 +81,24 @@ def createNote():
                             <td style="text-align: center;" colspan="2"><p><strong>Summary</strong></p><p style="text-align: left;">After class: Summary of the lesson and highlighting key points</p>
                             </td></tr></tbody></table>"""
             elif template == 'Mapping':
-                content = " "
+                content = """
+                    <p><strong>Main Topic</strong></p>
+                    <table style="width: 100%;">
+                        <tbody>
+                            <tr><th style="width: 33.3333%;">Subtopic 1</th><th style="width: 33.3333%;">Subtopic 2</th><th style="width: 33.3333%;">Subtopic 3</th></tr>
+                            <tr><th style="width: 33.3333%;">Key Details</th><th style="width: 33.3333%;">Subtopic 2</th><th style="width: 33.3333%;">Subtopic 3</th></tr>
+                            <tr><td style="width: 33.3333%;"><ol><li>Point 1</li><li>Point 2</li><li>Point 3</li></ol></td><td style="width: 33.3333%;"><ol><li>Point 1</li><li>Point 2</li>
+                            <li>Point 3</li></ol></td><td style="width: 33.3333%;"><ol><li>Point 1</li><li>Point 2</li><li>Point 3</li></ol></td></tr>
+                        </tbody>
+                    </table>
+                """
             elif template == 'Outlining':
                 content = """<p><strong>Main Topic</strong></p><ul><li>Subtopic 1<ul><li>Key point 1</li><li>Key point 2</li><li>Key point 3</li></ul></li></ul><ul><li>Subtopic 2<ul><li>Key point 1</li>
                             <li>Key point 2</li><li>Key point 3</li></ul></li></ul><ul><li>Subtopic 3<ul><li>Key point 1</li><li>Key point 2</li><li>Key point 3</li></ul></li></ul>"""
             elif template == 'Charting':
                 content = """<p><strong>Main Topic</strong></p><table style="width: 100%;"><tbody><tr><th style="width: 33.3333%;">Topic 1</th><th style="width: 33.3333%;">Topic 2</th><th style="width: 33.3333%;">Topic 3</th></tr>
                             <tr><td style="width: 33.3333%;"><ol><li>Point 1 Details</li><li>Point 2 Details</li><li>Point 3 Details</li></ol></td><td style="width: 33.3333%;"><ol><li>Point 1 Details</li><li>Point 2 Details</li>
-                            <li>Point 3 Details</li></ol></td><td style="width: 33.3333%;"><ol><li>Point 1 Details</li><li>Point 2 Details</li><li>Point 3 Details</li></ol></td></tr>"""
+                            <li>Point 3 Details</li></ol></td><td style="width: 33.3333%;"><ol><li>Point 1 Details</li><li>Point 2 Details</li><li>Point 3 Details</li></ol></td></tr></tbody></table>"""
             elif template == 'Sentence':
                 content = """<p><strong> Main Topic </strong></p><ol><li>Sentence covering key details of the topic</li><li>Sentence covering key details of the topic</li><li>Sentence covering key details of the topic</li></ol>"""
 
@@ -107,7 +117,7 @@ def viewNote(noteID):
 
     if selectedNote == None:
         return redirect(url_for('home'))
-    elif note['privacy'] == 'Private'  and  user == None:
+    elif selectedNote['privacy'] == 'Private'  and  user == None:
         return redirect(url_for('home'))
     elif user != None:
         if ObjectId(noteID) not in user['createdNotes']  and  ObjectId(noteID) not in user['receivedNotes']  and  user['admin'] != True:         #note id not in users's created or received notes and not admin
